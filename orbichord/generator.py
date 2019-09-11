3a6a01562b2df7a174dc7eb54c30ddcf0615a076
Victor Eduardo Bazterra <bazterra@gmail.com>
2019-09-11 16:04:44 -0400

    Improving docstring for generator.

--- orbichord/generator.py
+++ orbichord/generator.py
@@ -6,22 +6,39 @@ from music21.chord import Chord
 
 
 class Generator:
-    """TODO."""
+    """Generate the space of n-pitches chords.
+
+    Generate the space of n-pitches chords. The list of pitches to be used in
+    the generation needs to be provided when instantiating the generator.
+
+    It is assumed that chords are musical objects which are at least invariant
+    under
+        * scale pitch shifts (O),
+        * pitch permutation (P),
+        * pitch cardinality or dimension (C)
+
+    By default chords with repeated pitches are not identified as the same
+    chord. If you wish to do so, you can pass an identitying function that
+    provides a hashable object to classify different objects as the same chord.
+    On the first object of the identification will be yield by the generator.
+
+    You can further restrict the generation of chords by passing a filter
+    function that restricts the type of chords yielded by the generator.
+    """
 
     def __init__(self,
         pitches: list,
         dimension: int,
-        filter: typing.Callable[[Chord], bool] = None,
-        identify: typing.Callable[[Chord], str] = None
+        identify: typing.Callable[[Chord], str] = None,
+        filter: typing.Callable[[Chord], bool] = None
     ):
-        """
-        Initialize the generator (Constructor).
+        """Initialize the generator (Constructor).
 
         Parameters:
-        pitches (list): List of music21.pitch.Pitch
-        dimension (int): Dimension of the space
-        filter (Callable[Chord, str]): Function to filter chords
-        identify (Callable[Chord, str]): Funtion to indentify chords
+            pitches (list): List of music21.pitch.Pitch
+            dimension (int): Dimension of the space
+            identify (Callable[Chord, str]): Funtion to indentify chords
+            filter (Callable[Chord, str]): Function to filter chords
         """
         # Sanity checks
         if dimension <= 0:
@@ -53,11 +70,10 @@ class Generator:
         return self._filter
 
     def run(self) -> typing.Iterator[Chord]:
-        """
-        Generate a sequence of chords.
+        """Generate a sequence of chords.
 
-        Returns:
-        typing.Iterator[Chord]: an iterator to the chords in the space
+        Yields:
+            typing.Iterator[Chord]: an iterator to the chords in the space
         """
         # List of identified chords
         identitied_chords = set()

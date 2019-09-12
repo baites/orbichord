2f9d1a054863dcd20b69dbd49245c11546bbf0ca
Victor Eduardo Bazterra <bazterra@gmail.com>
2019-09-11 20:51:20 -0400

    Changing voice leading between scales using E major.

--- examples/efficient_voice_leading_b_scales.py
+++ examples/efficient_voice_leading_b_scales.py
@@ -1,6 +1,6 @@
 
 from music21.chord import Chord
-from music21.scale import MinorScale, ChromaticScale
+from music21.scale import MinorScale, MajorScale, ChromaticScale
 from numpy import inf
 from numpy import linalg as la
 from orbichord.chordinate import efficientVoiceLeading
@@ -10,7 +10,7 @@ from music21.pitch import Pitch
 
 reference_scale = ChromaticScale('C')
 D_minor_scale = MinorScale('D')
-C_sharp_minor_scale = MinorScale('C#')
+E_major_scale = MajorScale('E')
 
 # Create a chord using D minor scale pitches
 D_minor_scale = Chord(D_minor_scale.getPitches('D4', 'C5'))
@@ -18,14 +18,14 @@ print('D minor scale == ', D_minor_scale)
 # <music21.chord.Chord D4 E4 F4 G4 A4 B-4 C5>
 
 # Create a chord using C# minor scale pitches
-C_sharp_minor_scale = Chord(C_sharp_minor_scale.getPitches('C#4', 'B4'))
-print('C# minor scale == ', C_sharp_minor_scale)
+E_major_scale = Chord(E_major_scale.getPitches('E4', 'D#5'))
+print('E major scale == ', E_major_scale)
 # <music21.chord.Chord C#4 D#4 E4 F#4 G#4 A4 B4>
 
 # Compute efficient voice leading
 vl, dist = efficientVoiceLeading(
     D_minor_scale,
-    C_sharp_minor_scale,
+    E_major_scale,
     reference_scale,
     lambda delta: la.norm(delta, inf)
 )

07f72031b6e949abe7b004c7229aa9a089b270b0
Victor Eduardo Bazterra <bazterra@gmail.com>
2019-10-04 13:43:01 -0400

    Finish testing chordinate module.

--- test/test_chordinate.py
+++ test/test_chordinate.py
@@ -1,6 +1,8 @@
 
 from music21.chord import Chord
-from music21.scale import ChromaticScale, MajorScale
+from music21.scale import ChromaticScale, MinorScale, MajorScale
+from numpy import inf
+from numpy import linalg as la
 from orbichord.chordinate import *
 
 # Define global variables
@@ -41,3 +43,41 @@ def test_interscalarMatrix():
     ref_chord = Chord('F B D')
     delta = interscalarMatrix(chord, ref_chord, scale)
     assert delta == [[1, 1, 2], [3, -3, -3], [-1, -1, -1]]
+
+
+def test_VoiceLeading():
+    """Test VoiceLeading module class."""
+    D_minor_scale = MinorScale('D')
+    D_minor_scale = Chord(D_minor_scale.getPitches('D4', 'C5'))
+    E_major_scale = MajorScale('E')
+    E_major_scale = Chord(E_major_scale.getPitches('E4', 'D#5'))
+    scale = ChromaticScale('C')
+    voice_leading = VoiceLeading(
+        scale = ChromaticScale('C'),
+        metric = lambda delta: la.norm(delta, inf)
+    )
+    vl, dist = voice_leading(
+        D_minor_scale,
+        E_major_scale,
+    )
+    assert vl == [2, 2, 3, 2, 2, 3, 3]
+    assert dist == 3.0
+
+
+def test_EfficientVoiceLeading():
+    """Test EfficientVoiceLeading module class."""
+    D_minor_scale = MinorScale('D')
+    D_minor_scale = Chord(D_minor_scale.getPitches('D4', 'C5'))
+    E_major_scale = MajorScale('E')
+    E_major_scale = Chord(E_major_scale.getPitches('E4', 'D#5'))
+    scale = ChromaticScale('C')
+    voice_leading = EfficientVoiceLeading(
+        scale = ChromaticScale('C'),
+        metric = lambda delta: la.norm(delta, inf)
+    )
+    vl, dist = voice_leading(
+        D_minor_scale,
+        E_major_scale,
+    )
+    assert vl == [1, 1, 0, 1, 1, 0, 1]
+    assert dist == 1.0

14c11e9039b7c278782c79d156ab306ce0faf5c0
Victor Eduardo Bazterra <bazterra@gmail.com>
2019-10-07 13:57:45 -0400

    Updating trichord example.

---
+++ examples/C_major_trichord.py
@@ -0,0 +1,23 @@
+"""
+Example of generator primitives.
+
+This example generates the space of C chords identify by
+their ordered pitch class string that are also triads.
+"""
+
+from orbichord.generator import Generator
+from orbichord.symbol import chordSymbolFigure
+from music21.scale import MajorScale
+
+scale = MajorScale('C')
+
+chord_generator = Generator(
+    pitches = scale.getPitches('C','B')
+)
+
+for chord in chord_generator.run():
+    print('{} {} - {}'.format(
+        chord,
+        chord.orderedPitchClassesString,
+        chordSymbolFigure(chord, inversion=0)
+    ))
